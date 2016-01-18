93009a76ba3cea6092336cc4a1342bad80406f5e
jedmonds91011 <jedmonds91011@yahoo.com>
2016-01-18 13:00:39 -0800

    deleting files from repo branch

--- Assets/Editor/EditorScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEditor;
-using UnityEngine;
-using System.Collections;
-
-public class EditorScript : MonoBehaviour
-{
-    [MenuItem("Edit/Reset Playerprefs")]
-    public static void DeletePlayerPrefs() { PlayerPrefs.DeleteAll(); }
-}
